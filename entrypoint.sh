fd599926e377ec04f78842f11f9a927d8676be5a
Ruslan Kuprieiev <kupruser@gmail.com>
2019-11-17 02:42:49 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,6 @@ git config --global user.email "actions@github.com"
 git status
 
 if [ -f "./.pre-commit-config.yaml" ]; then
-    pip install pre-commit
     reformat=0
     pre-commit run --all-files || reformat=1
     echo $reformat
