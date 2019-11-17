10f4c63c88f2ef14167755053dc177645bc1f8dc
Ruslan Kuprieiev <kupruser@gmail.com>
2019-11-17 02:50:21 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,9 +17,13 @@ git config --global user.email "actions@github.com"
 git status
 
 if [ -f "./.pre-commit-config.yaml" ]; then
-    reformat=0
-    pre-commit run --all-files || reformat=1
-    echo $reformat
+    reformat=false
+    pre-commit run --all-files || reformat=true
+    if [[ "$reformat" == "true" ]]; then
+        git add .
+        git commit -m "Run pre-commit hooks"
+        git push
+    fi
 else
     echo "'pre-commit-config.yaml' is not found"
 fi
