bfc414fe74e00ac81bac3e3c3ebc0b9708d40e31
Ruslan Kuprieiev <kupruser@gmail.com>
2019-11-17 04:25:35 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,7 +25,7 @@ if [ -f "./.pre-commit-config.yaml" ]; then
         git checkout -b "$PR_NUM-reformatted"
         git add .
         git commit -m "Run pre-commit hooks"
-        git request-pull master ./
+        git request-pull origin/master ./
     fi
 else
     echo "'pre-commit-config.yaml' is not found"
